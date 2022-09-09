77bc6106d62acb5abb2effd54e0a2521c36cd3da
Konstantin Kochetkov <kmkochetkov@gmail.com>
2022-09-09 19:20:22 +0300

    Задача 19

---
+++ hw_c#_002_task_019/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+//Программа, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+void SecondNumber()
+{
+    Console.Write("Введите трехзначное число: ");
+    int number = Math.Abs(int.Parse(Console.ReadLine()));
+
+    // Проверка, является ли число трёхзначным
+    if (number < 100 || number > 999)
+    {
+        Console.WriteLine ("Введённое число не является трёхзначным! ");
+    }
+
+    // Вычленение и печать второй цифры
+    else
+    {
+        int secondNumber = number % 100 / 10;
+        Console.WriteLine($"Вторая цифра введённого числа: {secondNumber}");
+    }
+}
+SecondNumber();
